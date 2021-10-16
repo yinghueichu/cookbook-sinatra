aa5845ee25953d99a05ee4f07980007b3eea7935
yinghueichu <yinghuei.chu@essec.edu>
2021-10-16 17:34:05 +0200

    save

--- app.rb
+++ app.rb
@@ -6,7 +6,11 @@ configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
-
 get '/' do
-  'Hello world!/n test test'
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
+end
+
+get '/about' do
+  erb :about
 end
